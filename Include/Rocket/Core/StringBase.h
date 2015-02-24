47f952e6b1b64fb46445f9a9b2b953810b17e283
gm <gmisocpp@gmail.com>
2015-02-25 03:24:28 +1300

    Numerous performance and correctness changes to string handling. Requires C++11.
    * Formatting - change design to favour fixing thread and exception safety over performance.
    * Increase performance in some situations by the provision of move constructors and move assignment functions.
    * Attempt to reduce string memory reallocations for replacement and concatenation by avoiding temporaries.
    * Clear hash value where required such as in Erase.
    * Don't clear hash value if not required for Replace.
    * Null terminate in clear as if one does Assign("X") then Clear then CString() one expects null terminated string.
    * Don't use Clear in StringBase itself to favour concatenation performance, only release memory on destruction.
    * Hopefully make tolower/touppwer simpler/faster and reduce memory reallocation potential and preserve hash more often.
    
    Sorry for single commit.

--- Include/Rocket/Core/StringBase.h
+++ Include/Rocket/Core/StringBase.h
@@ -51,6 +51,8 @@ public:
 	StringBase(const T* string_start, const T* string_end);
 	StringBase(size_type length, const T character);
 	StringBase(size_type max_length, const T* fmt, ...);
+	// Move construct.
+	StringBase(StringBase<T>&& other);
 
 	~StringBase();
 
@@ -174,6 +176,8 @@ public:
 
 	inline StringBase<T>& operator=(const T* assign);
 	inline StringBase<T>& operator=(const StringBase<T>& assign);
+	// Move assign.
+	inline StringBase<T>& operator=(StringBase<T>&& rhs);
 
 	inline StringBase<T> operator+(const T* append) const;
 	inline StringBase<T> operator+(const StringBase<T>& append) const;
@@ -185,8 +189,19 @@ public:
 	inline const T& operator[](size_type index) const;
 	inline T& operator[](size_type index);
 
-protected:	
+	// Offer c_str() to make the interface somewhat more consistent with std::string.
+	// c_str() better sets expectations about what it does than CString()
+	// Having a consistent name with std::string also helps with generic code.
+	inline const T* c_str() const;
 
+protected:
+	// Obtain the local buffer without casting.
+	inline T* LocalBuffer();
+	inline const T* LocalBuffer() const;
+	// Is the Small String Optimization buffer in use.
+	inline bool IsUsingLocalBuffer() const;
+
+protected:
 	T* value;
 	size_type buffer_size;
 	size_type length;
@@ -210,7 +225,7 @@ protected:
 	inline StringBase<T>& _Assign(const T* assign, size_type assign_length, size_type count = StringBase<T>::npos);
 	inline void _Insert(size_type index, const T* insert, size_type insert_length, size_type count = StringBase<T>::npos);
 };
-	
+
 #include "StringBase.inl"
 
 }
--- Source/Core/String.cpp
+++ Source/Core/String.cpp
@@ -29,32 +29,36 @@
 #include "../../Include/Rocket/Core/String.h"
 #include "../../Include/Rocket/Core/StringBase.h"
 
+#include <memory>
+
 namespace Rocket {
 namespace Core {
 
 int ROCKETCORE_API RocketStringFormatString(StringBase<char>& string, int max_size, const char* format, va_list argument_list)
 {
-	const int INTERNAL_BUFFER_SIZE = 1024;
-	static char buffer[INTERNAL_BUFFER_SIZE];
-	char* buffer_ptr = buffer;
+	const int INTERNAL_BUFFER_SIZE = 2048;
+	int length = 0;
 
 	if (max_size + 1 > INTERNAL_BUFFER_SIZE)
-		buffer_ptr = new char[max_size + 1];
-
-	int length = vsnprintf(buffer_ptr, max_size, format, argument_list);
-	buffer_ptr[length >= 0 ? length : max_size] = '\0';
-	#ifdef ROCKET_DEBUG
-		if (length == -1)
-		{
-			Log::Message(Log::LT_WARNING, "String::sprintf: String truncated to %d bytes when processing %s", max_size, format);
-		}
-	#endif
-
-	string = buffer_ptr;
-
-	if (buffer_ptr != buffer)
-		delete[] buffer_ptr;
-
+	{
+		std::unique_ptr<char[]> buffer_ptr(new char[max_size + 1]);
+		int length = vsnprintf(buffer_ptr.get(), max_size, format, argument_list);
+		buffer_ptr[length >= 0 ? length : max_size] = '\0';
+		string = buffer_ptr.get();
+	}
+	else
+	{
+		char buffer[INTERNAL_BUFFER_SIZE];
+		int length = vsnprintf(buffer, max_size, format, argument_list);
+		buffer[length >= 0 ? length : max_size] = '\0';
+		string = buffer;
+	}
+#ifdef ROCKET_DEBUG
+	if (length == -1)
+	{
+		Log::Message(Log::LT_WARNING, "String::sprintf: String truncated to %d bytes when processing %s", max_size, format);
+	}
+#endif
 	return length;
 }
 
