55c1ba91bc4d6169242d13a2db0886e3dd796cd3
asarium <asarium@gmail.com>
2018-09-16 13:09:25 +0200

    Fix compilation using MinGW

--- Include/Rocket/Core/String.h
+++ Include/Rocket/Core/String.h
@@ -73,7 +73,7 @@ ROCKETCORE_API_INLINE bool StringBase< char >::operator!=(const char * compare) 
 }
 
 // Redefine Windows APIs as their STDC counterparts.
-#ifdef ROCKET_PLATFORM_WIN32
+#if defined(ROCKET_PLATFORM_WIN32) && defined(_MSC_VER)
 	#define strcasecmp stricmp
 	#define strncasecmp strnicmp
 #endif
